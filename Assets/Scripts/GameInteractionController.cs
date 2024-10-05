e7194bcc971de59cc23b329f96b81a01e8b6edb0
Johan Veenpere <johan.veenpere@gmail.com>
2024-10-05 18:11:02 +0300

    added scripts for food related actions

---
+++ Assets/Scripts/GameInteractionController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInteractionController : MonoBehaviour
+{
+    private List<FoodItem> foodItems;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}   
