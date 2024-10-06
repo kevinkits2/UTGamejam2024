e9c76293ee7f7b2c19e1fb22eec860c4932c17d5
Johan Veenpere <johan.veenpere@gmail.com>
2024-10-06 14:01:51 +0300

    dust lookat implemented

---
+++ Assets/Scripts/Dust.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dust : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.LookAt(Camera.main.transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
